6f64ac9aecbd2b4b4b87cf879e352da407af4b86
Jean-Massumi <geanmassumit@hotmail.com>
2024-06-05 13:31:12 -0300

    First commit

---
+++ PrimeiroProjeto/src/PrimeiroPrograma/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PrimeiroPrograma;
+
+/**
+ *
+ * @author geanm
+ */
+public class HelloWorld {
+    /**
+     * Este bloco permite executar a aplicação
+     * public: permite vizualizar a classe em outras classes
+     * static: usados em métodos e variáveis em que seus objetos
+     * não precisam ser instanciados
+     * void:o método não retorna nenhum tipo de dado
+     * main: nome do método para ser executado
+    */
+    
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        // 'System' é a classe,'out' é o objeto e 'println' é o método.
+        // Imprime a saída.
+    }
+}
+
