c1a69aab1f37ded4605a81abafd8709f36024129
Jean-Massumi <geanmassumit@hotmail.com>
2024-06-14 16:05:21 -0300

    Movendo pastas

---
+++ Exercicios_Aula3/ProgramaMedia/src/programamedia/ProgramaMedia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programamedia;
+
+/**
+ *
+ * @author geanm
+ */
+public class ProgramaMedia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
